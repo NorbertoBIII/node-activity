361c022161d1fddcbefac73f958be1f756db8ac3
NorbertoBIII <115629778+NorbertoBIII@users.noreply.github.com>
2022-12-02 21:11:01 +0800

    node act

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const port = 4001
+
+const server  = http.server.createServer(function(req, res){
+res.write("Hello World")
+res.end()
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Something went wrong', error)       
+    }
+    else
+    {
+        console.log('Server is listening to port' + port)
+    }
+})
